6ca8f591d6ab7f4b3b1cbbd76c46ca0adb5ea68e
megychen <megy.chen203@gmail.com>
2018-09-21 11:34:46 +0800

    Init project & webpack settings

---
+++ src/todo/todo.vue
@@ -0,0 +1,106 @@
+<template>
+  <section class="real-app">
+    <input
+      type="text"
+      class="add-input"
+      autofocus="autofocus"
+      placeholder="接下来要做什么？"
+      @keyup.enter="addTodo"
+      v-model="content"
+    >
+    <item
+      v-for="todo in filterTodos"
+      :todo="todo"
+      :key="todo.id"
+      @del="deleteTodo"
+    >
+    </item>
+    <tabs
+      :filter="filter"
+      :todos="todos"
+      @toggle="toggleFilter"
+      @clearAllCompleted="clearAllCompleted"
+    >
+    </tabs>
+  </section>
+</template>
+
+<script>
+import item from "./item.vue";
+import tabs from "./tabs.vue";
+
+export default {
+  name: "Todo",
+  components: {
+    item,
+    tabs
+  },
+  data() {
+    return {
+      todos: [],
+      id: 0,
+      content: "",
+      filter: "all"
+    };
+  },
+  computed: {
+    filterTodos() {
+      if (this.filter === "all") {
+        return this.todos;
+      }
+      const completed = this.filter === "completed" ? true : false;
+      return this.todos.filter(todo => completed === todo.completed);
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.unshift({
+        id: this.id++,
+        content: this.content,
+        completed: false
+      });
+      this.content = "";
+    },
+    deleteTodo(id) {
+      this.todos.splice(this.todos.findIndex(todo => todo.id === id), 1);
+    },
+    toggleFilter(state) {
+      this.filter = state;
+      console.log(this.filter);
+    },
+    clearAllCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .real-app {
+    width: 600px;
+    margin: 0 auto;
+    box-shadow: 0 0 5px #666;
+  }
+
+  .add-input {
+    position: relative;
+    margin: 0;
+    width: 100%;
+    font-size: 24px;
+    font-family: inherit;
+    font-weight: inherit;
+    line-height: 1.4em;
+    border: 0;
+    outline: none;
+    color: inherit;
+    padding: 6px;
+    border: 1px solid #999;
+    box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+    box-sizing: border-box;
+    font-smoothing: antialiased;
+    padding: 16px 16px 16px 60px;
+    border: none;
+    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+  }
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin") // 分离打包后的css内容
+const isDev = process.env.NODE_ENV  === 'development'
+
+const config = {
+  target: 'web',
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin()
+  ]
+}
+
+if (isDev) {
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
+  config.devtool = '#cheap-module-eval-source-map'
+  config.devServer = {
+    port: 8080,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true
+    },
+    hot: true
+  }
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  )
+} else {
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
+  config.output.filename = '[name].[chunkhash:8].js'
+
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+          // you can specify a publicPath here
+          // by default it use publicPath in webpackOptions.output
+          // publicPath: '../'
+        }
+      },
+      "css-loader",
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+        }
+      },
+      "stylus-loader"
+    ]
+  })
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'styles.[contentHash:8].css'
+    }),
+    new webpack.optimize.RuntimeChunkPlugin({
+      name: "vendor"
+    }),
+    new webpack.optimize.RuntimeChunkPlugin({
+        name: "runtime"
+    })
+  )
+}
+
+module.exports = config
